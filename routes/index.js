b3641765b315fc5a96da7ca1f1b7817094b5fd32
BHPriyanka <BHPriyanka@ccs.neu.edu>
2018-03-04 15:18:19 -0800

    JSON endpoints and creating mongodb indexes

--- routes/index.js
+++ routes/index.js
@@ -54,4 +54,10 @@ router.get('/account/reset/:token', catchErrors(authController.reset));
 router.post('/account/reset/:token', authController.confirmedPasswords, 
 catchErrors(authController.update));
 
+
+/*
+ API
+*/
+router.get('/api/search', catchErrors(storeController.searchStores));
+
 module.exports = router;
