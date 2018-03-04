da433dc780cd4413bdb5a4b830a11a0cded9cbe2
BHPriyanka <BHPriyanka@ccs.neu.edu>
2018-03-04 15:55:50 -0800

    Implementation of compelete search results feature

--- public/javascripts/modules/typeAhead.js
+++ public/javascripts/modules/typeAhead.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
+import dompurify from 'dompurify';
 
 function searchResultsHTML(stores) {
   return stores.map(store => {
     return `
-     <a href="/stores/${store.slug}" class="search__result">
+     <a href="/store/${store.slug}" class="search__result">
        <strong>${store.name}</strong>
      </a>
      `;
@@ -25,19 +26,52 @@ function typeAhead(search) {
 
     // show the search result!
     searchResults.style.display = 'block';
-    searchResults.innerHTML = '';
 
     axios
       .get(`/api/search?q=${this.value}`)
       .then(res => {
         if (res.data.length) {
-          const html = searchResultsHTML(res.data);
-          searchResults.innerHTML = searchResultsHTML(res.data);
-        }
+          searchResults.innerHTML = dompurify.sanitize(searchResultsHTML(res.data));
+          return;
+        } 
+
+        // tell them nothing came back
+        searchResults.innerHTML = dompurify.sanitize(`<div class="search__result">No resuts for ${this.value} found!</div>`);
       })
       .catch(err => {
         console.error(err);
       });
+  });
+
+  // handle keyboard inputs
+  searchInput.on('keyup', (e) => {
+    // if they aren't pressing up, down and enter, who cares!
+    if ( ![38, 40, 13].includes(e.keyCode)) {
+      return; //skip it
+    }
+    
+    const activeClass = 'search__result--active';
+    const current = search.querySelector(`.${activeClass}`);
+    const items = search.querySelectorAll('.search__resut');
+    let next;
+    
+    if (e.keyCode === 40 && current) {
+      next = current.nextElementSibling || items[0];
+    } else if(e.keyCode === 40) {
+      next = items[0];
+    } else if (e.keyCode === 38 && current) {
+      next = current.previousElementSibling || items[items.length-1]
+    } else if (e.keyCode === 38 ){
+      next = items[items.length-1];
+    } else if (e.keyCode === 13 && current.href) {
+      window.location = current.href;
+      return;
+    }
+    
+    if (current) {
+      current.classList.remove(activeClass);
+    }
+    next.classList.add(activeClass);
   });
 };
 
