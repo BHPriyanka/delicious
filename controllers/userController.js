0773ed8010e713e62d031aeb524d0e6a05945d2b
BHPriyanka <BHPriyanka@ccs.neu.edu>
2018-03-04 12:56:40 -0800

    Sending email with nodejs using mailtrap.io

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,11 +14,11 @@ exports.validateRegister = (req, res, next) => {
   req.sanitizeBody('name');
   req.checkBody('name', 'You must supply a name!').notEmpty();
   req.checkBody('email', 'That Email is not valid!').isEmail();
-  req.sanitizeBody('email').normalizeEmail({ 
+/*  req.sanitizeBody('email').normalizeEmail({ 
     remove_dots: false,
     remove_extension: false,
     gmail_remove_subaddress: false
-  });
+  });*/
    req.checkBody('password', 'Password cannot be blank!').notEmpty();
    req.checkBody('password-confirm', 'Confirmed password cannot be blank!').notEmpty();
    req.checkBody('password-confirm', 'Oops! Your passwords do not match').equals(req.body.password);
