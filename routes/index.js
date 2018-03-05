7292f68f69f3fe991be811dd61e40a8a50d8683d
BHPriyanka <BHPriyanka@ccs.neu.edu>
2018-03-05 14:26:39 -0800

    Added data model for reviews

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -55,6 +56,7 @@ router.post('/account/reset/:token', authController.confirmedPasswords,
 catchErrors(authController.update));
 
 router.get('/map', storeController.mapPage);
+router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview));
 
 /*
  API
